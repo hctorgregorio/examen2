fc3b0d86c1ef7dbedafd2479acb0befacc62c1b1
Hector <Hector@T104-03>
2019-04-02 11:56:32 +0100

    fg

---
+++ src/examen/Ejercicios.java
@@ -0,0 +1,7 @@
+package examen;
+// 2 de abril de 2019
+public class Ejercicios {
+	
+	// implementar y llamar desde AQUI los ejercicios
+    // ¡IMPORTANTE!: NO DEJAR AQUI CÓDIGO IRRELEVANTE, LEER ENUNCIADO.
+}
---
+++ src/modelo/Equipo.java
@@ -0,0 +1,18 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Equipo  {
+	// completar la clase con Constructor, getter/setters.
+	private int id;
+	private String nombreCorto;
+	private String nombre;
+	private int pj;
+	private int puntos;
+	private int pg;
+	private int pe;
+	private int pp;
+	private int gf;
+	private int gc;
+
+}
