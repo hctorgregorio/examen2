e4bf983b0d31f29170a8cec655541437186b8c14
Hector <Hector@DESKTOP-VLN4OL6.Home>
2019-04-04 13:05:29 +0200

    final

--- src/examen/Ejercicios.java
+++ src/examen/Ejercicios.java
@@ -1,7 +1,155 @@
 package examen;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import modelo.Equipo;
+import modelo.Jugador;
+
 // 2 de abril de 2019
 public class Ejercicios {
 	
 	// implementar y llamar desde AQUI los ejercicios
     // ¡IMPORTANTE!: NO DEJAR AQUI CÓDIGO IRRELEVANTE, LEER ENUNCIADO.
+	
+	public static void main(String[] args) {
+		
+		Ejercicios ejercicio = new Ejercicios();
+		String rutaJugadores = "ficheros/jugadores.txt";
+		String rutaEquipos = "ficheros/equipos.txt";
+		String idEquipo = "1";
+		
+		//ejercicio 1
+		ArrayList<Jugador> listaJugadores = ejercicio.ejercicio1(rutaJugadores, idEquipo);
+		//ejercicio 2
+		HashMap<String, ArrayList<Jugador>> mapaJugadoresPorEqupos = ejercicio.ejercicio2(rutaJugadores);
+		//ejercicio 3_1
+		ArrayList<Equipo> listaEquiposOrdenada = ejercicio.ejercicio3_1(rutaEquipos);
+		//ejercicio 3_2
+		ejercicio.ejercicio3_2(listaEquiposOrdenada);
+		
+		System.out.println("FIN DEL PROGRAMA");
+		
+	}
+	
+	public ArrayList<Jugador> ejercicio1 (String rutaJugadores , String equipo) {
+		
+		ArrayList<Jugador> listaJugadores = new ArrayList<Jugador>();
+		
+		try {
+			BufferedReader ficheroJugadores = new BufferedReader(new FileReader(rutaJugadores));
+			String jugador;
+			
+			while ((jugador = ficheroJugadores.readLine()) != null) {
+				
+				String[] datosJugador = jugador.split("#");
+				
+				if (datosJugador[7].compareTo(equipo) == 0) 
+					listaJugadores.add(introducirJugador(datosJugador));	
+
+			}
+			
+			ficheroJugadores.close();
+			System.out.println("Fin de la lectura del fichero");
+
+		} catch (FileNotFoundException excepcion) {
+			System.out.println("fichero no encontrado");
+			return null;
+
+		} catch (IOException e) {
+			System.out.println("IO Excepcion");
+		}
+		
+		return listaJugadores;
+	}
+
+	public HashMap<String, ArrayList<Jugador>> ejercicio2 (String rutaJugadores) {
+		
+		int numEquipos = 20;
+		HashMap<String, ArrayList<Jugador>> mapaJugadoresPorEqupos = new HashMap<String, ArrayList<Jugador>>();
+		
+		for (int i = 0; i < numEquipos; i++) {
+			mapaJugadoresPorEqupos.put(Integer.toString(i + 1), ejercicio1(rutaJugadores, Integer.toString(i + 1)));
+		}
+		
+		return mapaJugadoresPorEqupos;
+	}
+
+	public ArrayList<Equipo> ejercicio3_1 (String rutaEquipos) {
+		
+		ArrayList<Equipo> listaEquipos = crearListaEquipos(rutaEquipos);
+		listaEquipos.sort(null);
+		 
+		return listaEquipos;
+	}
+	
+	public void ejercicio3_2 (ArrayList<Equipo> listaEquipos) {
+		for (Equipo equipo : listaEquipos) {
+			
+			System.out.println("id: " + equipo.getId() + 
+							   " nombre corto: " + equipo.getNombreCorto() + 
+							   " nombre: " + equipo.getNombre());
+		}
+	}
+
+	public Jugador introducirJugador (String[] datosJugador) {
+		
+		Jugador jugador = new Jugador();
+		
+		jugador.setId(Integer.parseInt(datosJugador[0]));
+		jugador.setNif(datosJugador[1]);
+		jugador.setNombre(datosJugador[2]);
+		jugador.setLongitudPaso(Integer.parseInt(datosJugador[3]));
+		jugador.setFecha_nac(datosJugador[4]);
+		jugador.setSexo(datosJugador[5].charAt(0));
+		jugador.setDorsal(Integer.parseInt(datosJugador[6]));
+		jugador.setIdEquipo(Integer.parseInt(datosJugador[7]));
+
+		return jugador;
+	}
+	
+	public Equipo introducirEquipo (String[] datosEquipo) {
+		
+		Equipo equipo = new Equipo();
+		
+		equipo.setId(Integer.parseInt(datosEquipo[0]));
+		equipo.setNombreCorto(datosEquipo[1]);
+		equipo.setNombre(datosEquipo[2]);
+
+		return equipo;
+	}
+	
+public ArrayList<Equipo> crearListaEquipos (String rutaEquipos) {
+		
+		ArrayList<Equipo> listaEquipos = new ArrayList<Equipo>();
+		
+		try {
+			BufferedReader ficheroEquipos = new BufferedReader(new FileReader(rutaEquipos));
+			String equipo;
+			
+			while ((equipo = ficheroEquipos.readLine()) != null) {
+				
+				String[] datosEquipo = equipo.split("#");
+				listaEquipos.add(introducirEquipo(datosEquipo));
+
+			}
+			
+			ficheroEquipos.close();
+			System.out.println("Fin de la lectura del fichero");
+
+		} catch (FileNotFoundException excepcion) {
+			System.out.println("fichero no encontrado");
+			return null;
+
+		} catch (IOException e) {
+			System.out.println("IO Excepcion");
+		}
+		
+		return listaEquipos;
+	}
 }
--- src/modelo/Equipo.java
+++ src/modelo/Equipo.java
@@ -2,7 +2,7 @@ package modelo;
 
 import java.io.Serializable;
 
-public class Equipo  {
+public class Equipo implements Serializable,Comparable<Equipo>{
 	// completar la clase con Constructor, getter/setters.
 	private int id;
 	private String nombreCorto;
@@ -14,5 +14,95 @@ public class Equipo  {
 	private int pp;
 	private int gf;
 	private int gc;
+	
+	public Equipo(int id, String nombreCorto, String nombre, int pj, int puntos, int pg, int pe, int pp, int gf,
+			int gc) {
+		super();
+		this.id = id;
+		this.nombreCorto = nombreCorto;
+		this.nombre = nombre;
+		this.pj = pj;
+		this.puntos = puntos;
+		this.pg = pg;
+		this.pe = pe;
+		this.pp = pp;
+		this.gf = gf;
+		this.gc = gc;
+	}
+	
+	public Equipo() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombreCorto() {
+		return nombreCorto;
+	}
+	public void setNombreCorto(String nombreCorto) {
+		this.nombreCorto = nombreCorto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPj() {
+		return pj;
+	}
+	public void setPj(int pj) {
+		this.pj = pj;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	public int getPg() {
+		return pg;
+	}
+	public void setPg(int pg) {
+		this.pg = pg;
+	}
+	public int getPe() {
+		return pe;
+	}
+	public void setPe(int pe) {
+		this.pe = pe;
+	}
+	public int getPp() {
+		return pp;
+	}
+	public void setPp(int pp) {
+		this.pp = pp;
+	}
+	public int getGf() {
+		return gf;
+	}
+	public void setGf(int gf) {
+		this.gf = gf;
+	}
+	public int getGc() {
+		return gc;
+	}
+	public void setGc(int gc) {
+		this.gc = gc;
+	}
 
+	@Override
+	public int compareTo(Equipo eq) {
+		if (this.getNombre().compareToIgnoreCase(eq.getNombre()) > 0)
+			return 1;
+		else if (this.getNombre().compareToIgnoreCase(eq.getNombre()) < 0)
+			return -1;
+		else
+			return 0;
+	}
+	
 }
